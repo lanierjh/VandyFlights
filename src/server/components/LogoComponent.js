cc017aeeedf8f07f9c09045101317cb818a24429
James Huang <james153018@gmail.com>
2024-09-24 12:02:03 -0500

    add updates

---
+++ src/server/components/LogoComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LogoComponent.css';
+
+const LogoComponent = () => {
+  return (
+    <div className="vandy-logo">
+      <span>V</span>
+      <span className="airplane-icon">✈️</span>
+    </div>
+  );
+};
+
+export default LogoComponent;
