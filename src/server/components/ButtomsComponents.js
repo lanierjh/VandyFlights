0a6bce29887bd3f97e891987334e5e7b3d550ee5
James Huang <james153018@gmail.com>
2024-09-24 12:02:03 -0500

    add updates

---
+++ src/server/components/ButtomsComponents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ButtonsComponent.css';
+
+const ButtonsComponent = () => {
+  return (
+    <div className="buttons-container">
+      <button className="login-btn">Log In</button>
+      <button className="register-btn">Register</button>
+    </div>
+  );
+};
+
+export default ButtonsComponent;
---
+++ src/server/components/ImageComponents.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageComponent.css';
+
+const ImageComponent = () => {
+  return (
+    <div className="image-container">
+      <img src="https://your-image-url" alt="Vanderbilt Tower" className="tower-image"/>
+    </div>
+  );
+};
+
+export default ImageComponent;
