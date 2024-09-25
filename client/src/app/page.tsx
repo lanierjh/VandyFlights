ef11f244c0d7e317a4e7490fa15f38fb05e09f52
James Huang <james153018@gmail.com>
2024-09-25 13:01:57 -0500

    add more UI to landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 
 export default function Home() {
   const homeContent = {
-    height: 50 + "vh",
-    marginTop: 25 + "vh"
+    height: '50vh',
+    marginTop: '25vh',
   };
 
   const rightSide = {
@@ -14,7 +14,6 @@ export default function Home() {
 
   const logInButton = {
     width: "30%",
-
     backgroundColor: "#1e3a8a",
     color: "white",
     padding: "15px 30px",
@@ -39,29 +38,65 @@ export default function Home() {
     transition: "all 0.3s ease",
   }
 
+
+  const vanderbiltPlaneRow = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '200px',
+  };
+
   return (
     <div className="row" id="HomeContent" style={homeContent}>
       <div className="col-sm-2"></div>
-      <div className="col-sm-4">
+      <div className="col-sm-3">
         <div >
           <Image
             src="/zeppos.jpg"
             alt="Vanderbilt Tower"
             width={300}
             height={450}
-            
-          />
+              style={{ borderRadius: "20px" }}
+            />
+          </div>
         </div>
+        <div className="col-sm-1">
+          <div style={vanderbiltPlaneRow}>
+            <Image
+              src="/vanderbilt.png"
+              alt="Vanderbilt Logo"
+              width={100}
+              height={50}
+            />
+
+            <svg height="600" width="300">
+            <path
+              d="M0 250 Q 50 0 65 250"
+              stroke="gray"
+              strokeWidth="4"
+              fill="transparent"
+              strokeDasharray="5,5"
+            />
+          </svg>
+
+          <Image
+            src="/plane.png"
+            alt="Airplane Logo"
+            width={100}
+            height={50}
+          />
+          </div>
       </div>
       <div className="col-sm-4 text-center" style={rightSide}>
-        <h1 className=" my-5">VandyFlights</h1>
-        <div className=" my-5">
-          <button className = "p-3" style={logInButton}>Log In</button>
+        <h1 className="my-5" style={{ fontWeight: 'bold' }}>VandyFlights</h1>
+        <div className="my-5">
+          <button className="p-3" style={logInButton}>Log In</button>
         </div>
-        <div className=" my-5">
-          <a href = "/register"><button className = "p-3" style={registerButton}>Register</button></a>
+        <div className="my-5">
+          <a href="/register">
+            <button className="p-3" style={registerButton}>Register</button>
+          </a>
         </div>
-        
       </div>
     </div>
   );
