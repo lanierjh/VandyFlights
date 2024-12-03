16bdee070d539ea854a34a0f9569a652a88301ec
Jackson Lanier <jackson.h.lanier@vanderbilt.edu>
2024-12-03 11:36:13 -0600

    taking out app

--- core/main.py
+++ core/main.py
@@ -2,7 +2,7 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
-from core.routers import user, auth, flights, app
+from core.routers import user, auth, flights
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
